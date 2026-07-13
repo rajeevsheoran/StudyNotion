39f7015cfcdf9480041d60b7ee0ab8683e7ae016
rajeevsheoran <rajeevshoran7@gmail.com>
2026-07-13 17:16:24 +0530

    Removing error

--- server/config/database.js
+++ server/config/database.js
@@ -2,6 +2,9 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 exports.connect = () => {
+ // console.log(process.env.MONGODB_URL);
+ console.log("MONGODB_URL =", process.env.MONGODB_URL);
+
     mongoose.connect(process.env.MONGODB_URL, {
         useNewUrlParser: true,
         useUnifiedTopology:true,
